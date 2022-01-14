675f2a87a4d47a42f620dd774ab8dbee71d7c741
criscardMDX <Angeloso1!>
2022-01-15 00:09:54 +0400

    This is my first attempt at a navigation stack

---
+++ catkin_ws/build/ccmdx1_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cristiano/ROS/catkin_ws/src/ccmdx1_navigation
+# Build directory: /home/cristiano/ROS/catkin_ws/build/ccmdx1_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
